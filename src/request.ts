84189df698608624d7f1d43d5c10f14b0ee82c45
Li yajun <1223679132@qq.com>
2025-03-06 19:22:00 +0800

    feat(request): 添加生产环境的 base URL 并定义开发环境变量- 新增 isDev 变量以判断当前是否为开发环境- 在 axios 创建时根据环境动态设置 base URL - 开发环境使用本地地址，生产环境使用云端地址

--- src/request.ts
+++ src/request.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
+export const isDev: boolean = process.env.NODE_ENV === "development";
+
 const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
+  baseURL: isDev
+    ? "http://localhost:8101"
+    : "https://yadada-backend-144682-5-1324877504.sh.run.tcloudbase.com/",
   timeout: 60000,
   withCredentials: true,
 });
